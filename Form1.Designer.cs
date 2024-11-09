0849f92f55a73e8863b3f0670718d7e38112b96b
juscodec <botcurioso.net@gmail.com>
2024-11-09 16:50:33 -0500

    Battery charge Monitor

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BatteryMonitor
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownLimit = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblBatteryPercentage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLimit)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numericUpDownLimit
+            // 
+            this.numericUpDownLimit.Location = new System.Drawing.Point(337, 23);
+            this.numericUpDownLimit.Name = "numericUpDownLimit";
+            this.numericUpDownLimit.Size = new System.Drawing.Size(89, 22);
+            this.numericUpDownLimit.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(229, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblBatteryPercentage
+            // 
+            this.lblBatteryPercentage.AutoSize = true;
+            this.lblBatteryPercentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBatteryPercentage.Location = new System.Drawing.Point(3, 0);
+            this.lblBatteryPercentage.Name = "lblBatteryPercentage";
+            this.lblBatteryPercentage.Size = new System.Drawing.Size(203, 20);
+            this.lblBatteryPercentage.TabIndex = 2;
+            this.lblBatteryPercentage.Text = "Nivel de batería actual es:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(89, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(444, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Con esta APP cuide su batería de laptop";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingresar porcentaje de batería para alertar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(12, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Power By Jus Coder";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.lblBatteryPercentage);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.numericUpDownLimit);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(94, 149);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(479, 58);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 403);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Battery Charge Monitor";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLimit)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownLimit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblBatteryPercentage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media; // Necesario para reproducir sonido
+
+namespace BatteryMonitor
+{
+    public partial class form1 : Form
+    {
+        private int batteryLimit = 90; // Valor predeterminado
+        private Timer checkBatteryTimer;
+
+        public form1()
+        {
+            InitializeComponent();
+
+            // Configura el valor inicial en el NumericUpDown de la UI
+            numericUpDownLimit.Value = batteryLimit;
+
+            // Configuración del Timer para verificar el nivel de batería cada minuto
+            checkBatteryTimer = new Timer();
+            checkBatteryTimer.Interval = 60000; // 60,000 ms = 1 minuto
+            checkBatteryTimer.Tick += CheckBatteryLevel;
+            checkBatteryTimer.Start();
+
+            // Llama a UpdateBatteryPercentage() para mostrar el porcentaje de batería al abrir el programa
+            UpdateBatteryPercentage();
+        }
+
+        // Método para verificar el nivel de batería y mostrar una alerta si se alcanza el límite
+        private void CheckBatteryLevel(object sender, EventArgs e)
+        {
+            UpdateBatteryPercentage(); // Actualiza el porcentaje en el Label cada vez que el Timer se activa
+
+            var batteryStatus = SystemInformation.PowerStatus;
+            int batteryLevel = (int)(batteryStatus.BatteryLifePercent * 100);
+
+            if (batteryStatus.PowerLineStatus == PowerLineStatus.Online && batteryLevel >= batteryLimit)
+            {
+                // Reproduce un sonido personalizado en bucle continuo
+                SoundPlayer player = new SoundPlayer(@"C:\Users\Equipo\Downloads\siren-alert-96052.wav");
+                try
+                {
+                    player.PlayLooping(); // Reproduce el sonido en un bucle infinito
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al reproducir el sonido: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Muestra el mensaje de advertencia y espera a que el usuario cierre el cuadro de diálogo
+                MessageBox.Show("Desconecta el cargador, la batería ha alcanzado el límite de carga.", "Alerta de Batería", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                // Detiene el sonido una vez que se cierra el cuadro de mensaje
+                player.Stop();
+            }
+        }
+
+
+
+
+        // Método para actualizar el porcentaje de batería en el Label
+        private void UpdateBatteryPercentage()
+        {
+            var batteryStatus = SystemInformation.PowerStatus;
+            int batteryLevel = (int)(batteryStatus.BatteryLifePercent * 100);
+            lblBatteryPercentage.Text = $"Porcentaje de Batería: {batteryLevel}%";
+        }
+
+        // Método que se ejecuta al hacer clic en el botón de guardar
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            batteryLimit = (int)numericUpDownLimit.Value;
+            MessageBox.Show($"Límite de carga configurado al {batteryLimit}%", "Configuración Guardada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
